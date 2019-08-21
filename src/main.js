01a3f1adac5d3f8d1859e00f50e9c9cfa061b78b
cliu032 <chauncely@gmail.com>
2019-08-21 23:17:47 +1000

    use prismic

--- src/main.js
+++ src/main.js
@@ -4,8 +4,17 @@ import './plugins/vuetify';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import PrismicVue from 'prismic-vue'
+import linkResolver from './prismic/link-resolver'
+import htmlSerializer from './prismic/html-serializer'
 
 Vue.config.productionTip = false;
+
+Vue.use(PrismicVue, {
+  endpoint: window.prismic.endpoint,
+  linkResolver,
+  htmlSerializer
+})
 
 new Vue({
     router,
